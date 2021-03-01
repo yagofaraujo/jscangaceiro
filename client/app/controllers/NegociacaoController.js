9d5cff829b43bc65cbbd233714ba1aadc246c313
Yago Ferreira <yagofdearaujo@gmail.com>
2021-02-28 21:58:17 -0300

    Capítulo 05

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -1,38 +1,38 @@
 class NegociacaoController {
 
-  constructor() {
-    /* 
-      Precisamos usar o .bind pq querySelector só está presente no contexto
-      de 'document'. Dessa forma, precisamos atribuir o contexto do .this 
-      da variável '$' (que é uma função - funções têm seu próprio .this)
-      para que seja o 'document' através da função bind
-    */
-    let $ = document.querySelector.bind(document)
+    constructor() {
 
-    this._inputData = $('#data')
-    this._inputQuantidade = $('#quantidade')
-    this._inputValor = $('#valor')
-  }
+        let $ = document.querySelector.bind(document);
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+        this._negociacoes = new Negociacoes()
+    }
 
-  adiciona(event) {
-    event.preventDefault()
+    adiciona(event) {
 
-    /* outro jeito de transformar a string para data
-       let data = new Date(this._inputData.value.replace(/-/g, ',')) 
-    */
+        event.preventDefault();
 
-    /* outro jeito de transformar a string para data
-      let data = new Date(this._inputData.value.split('-'))   
-    */
+        const negociacao = this._criarNegociacao()
+        
+        this._negociacoes.adiciona(negociacao)
+        this._limpaFormulario()
+    }
 
-    let negociacao = new Negociacao(
-      DateConverter.paraData(this._inputData.value),
-      this._inputQuantidade.value,
-      this._inputValor.value
-    )
+    _limpaFormulario() {
 
-    let diaMesAno = DateConverter.paraTexto(negociacao.data)
+        this._inputData.value = ''
+        this._inputQuantidade.value = 1
+        this._inputValor.value = 0.0
+        this._inputData.focus()
+    }
 
-    console.log(diaMesAno)
-  }  
+    _criarNegociacao() {
+        
+        return new Negociacao(
+            DateConverter.paraData(this._inputData.value),
+            parseInt(this._inputQuantidade.value),
+            parseFloat(this._inputValor.value)
+        );
+    }
 }
--- client/app/ui/converters/DateConverter.js
+++ client/app/ui/converters/DateConverter.js
@@ -1,18 +1,20 @@
 class DateConverter {
-  
-  constructor() {
-    throw new Error('Esta classe não pode ser instanciada.')  //caso tente instanciar essa classe, apresentará esse erro
-  }
 
-  static paraTexto(data) {
-    return `${data.getDate()}/${
-      data.getMonth() + 1}/${data.getFullYear()}`
-  }
+    constructor() {
 
-  static paraData(texto) {
-    if(!/^\d{4}-\d{2}-\d{2}$/.test(texto))
-      throw new Error('Deve estar no formato aaaa-mm-dd')
-      
-    return new Date(...texto.split('-').map((item, indice) => item - indice % 2))
-  }
+        throw new Error('Esta classe não pode ser instanciada');
+    }
+
+    static paraTexto(data) {
+
+        return `${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()}`;
+    }
+
+    static paraData(texto) {
+
+        if (!/^\d{4}-\d{2}-\d{2}$/.test(texto))
+            throw new Error('Deve estar no formato aaaa-mm-dd');
+
+        return new Date(...texto.split('-').map((item, indice) => item - indice % 2));
+    }
 }
